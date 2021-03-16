127a0ad0051121c61421b254cb4dbd266b67e192
wxrdnx <67510189+wxrdnx@users.noreply.github.com>
2021-03-16 21:02:00 +0800

    Update prometheus.py

--- prometheus.py
+++ prometheus.py
@@ -15,26 +15,33 @@ def write_to_csv(configs, url, out_dir):
     pp = pprint.PrettyPrinter(indent = 4)
 
     for config in configs:
+        # retrieve configuration
         ip = config['ip']
         exporter = config['exporter']
         probe = config['probe']
         metrics = config['metrics']
         write_metrics = config['write_metrics']
         csv_file = os.path.join(out_dir, probe + '@' + title_time + '.csv')
+        
+        # Query prometheus server metrics that appear in the configuration file
         for metric_name, metric_query in metrics.items():
             prom_url = url + '/api/v1/query?query=' + urllib.parse.quote(metric_query)
             with urllib.request.urlopen(prom_url) as res:
                 metrics_json = json.loads(res.read().decode())
+            # The quering result is in JSON format
             result = metrics_json['data']['result']
+            # check if the query is successful
             if metrics_json['status'] == 'success':
                 for res in result:
                     submetric_field = []
+                    # Write every metric label (submetric) that you want to write into the csv file
                     for submetric in write_metrics:
                         if not submetric in res['metric']:
                             break
                         else:
                             submetric_field.append(res['metric'][submetric])
                     else:
+                        # write value to csv file
                         value = res['value']
                         with open(csv_file, 'a', newline = '') as csvfile:
                             csv_writer = csv.writer(csvfile)
@@ -50,5 +57,6 @@ def wait_till_second(second):
 if __name__ == '__main__':
     with open('config.json') as f:
         configs = json.load(f)
-    write_to_csv(configs['configs'], configs['url'], configs['out_Dir'])
-
+    while True:
+        wait_till_second(0)
+        write_to_csv(configs['configs'], configs['url'], configs['out_Dir'])
